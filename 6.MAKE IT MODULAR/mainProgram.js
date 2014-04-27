64703dc025f207953280a59cbe6543a5c9abe2a7
Tomilla <gero.rim@gmail.com>
2014-04-27 23:41:03 +0800

    6th exercise COMPLETED

---
+++ 6.MAKE IT MODULAR/mainProgram.js
@@ -0,0 +1,56 @@
+var myModule = require('./myModule')
+var dirName = process.argv[2]
+var extName = process.argv[3]
+
+myModule(dirName, extName, function(err, data){
+    if (err)
+        throw err
+    data.forEach (function (file) {
+        console.log(file)
+    })
+})
+
+/*
+ *Here's the official solution is if you want to compare notes:
+ *
+ *────────────────────────────────────────────────────────────────────────────────
+ *solution.js:
+
+
+    var filterFn = require('./solution_filter.js')
+    var dir = process.argv[2]
+    var filterStr = process.argv[3]
+    
+    filterFn(dir, filterStr, function (err, list) {
+      if (err)
+        return console.error('There was an error:', err)
+    
+      list.forEach(function (file) {
+        console.log(file)
+      })
+    })
+
+ *────────────────────────────────────────────────────────────────────────────────
+ *solution_filter.js:
+
+
+    var fs = require('fs')
+    var path = require('path')
+    
+    module.exports = function (dir, filterStr, callback) {
+    
+      fs.readdir(dir, function (err, list) {
+        if (err)
+          return callback(err)
+    
+        list = list.filter(function (file) {
+          return path.extname(file) === '.' + filterStr
+        })
+    
+        callback(null, list)
+      })
+    }
+
+ *────────────────────────────────────────────────────────────────────────────────
+**/
+
