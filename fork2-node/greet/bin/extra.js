7ba49f0425b575569b7a1ba57b6c7b9dc30cf862
Tomilla <gero.rim@gmail.com>
2014-04-28 21:38:02 +0800

    remove 'extra.js', rewrite 'index.js'

--- fork2-node/greet/bin/extra.js
+++
@@ -1,8 +0,0 @@
-module.exports = function greet(inputName, drunk) {
-    if (drunk) {
-        return 'Hello ' + inputName + ', you look so sexy today'
-    }
-    else {
-        return "Hello " + inputName
-    }
-}
