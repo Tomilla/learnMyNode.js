2797b5a7cefb06bac3537eb3d183c7db27c391fa
Tomilla <gero.rim@gmail.com>
2014-04-28 21:26:43 +0800

    Revert "remove extra.js"
    This reverts commit 19a3e98a2dbfccd27f6e6a1352fbcb21a5162fb2.

--- fork2-node/greet/bin/greet.js
+++ fork2-node/greet/bin/greet.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-var greet = require('../index.js')
+var greet = require('./extra.js')
 var parseArgs = require('minimist')
 var name = process.argv[2]
 
 if (process.argv[3] != null)
-    var drunk = parseArgs(process.argv[3])
+    var drunk = parseArgs(process.argv[3].replace('-',''))
 
 console.log(greet(name, drunk))
--- fork2-node/greet/index.js
+++ fork2-node/greet/index.js
@@ -1,8 +1,3 @@
-module.exports = function greet(inputName, drunk) {
-    if (drunk) {
-        return 'Hello ' + inputName + ', you look so sexy today'
-    }
-    else {
-        return "Hello " + inputName
-    }
+module.exports = function greet(process.argv[2]){
+    console.log('hello' + process.argv[2])
 }
