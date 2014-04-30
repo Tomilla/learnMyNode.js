775aa7ff1e911e8785de7636b8657e80c932eff3
Tomilla <gero.rim@gmail.com>
2014-04-30 13:06:42 +0800

    Convert 'bin/greet.js'

--- fork2-node/greet/bin/greet.js
+++ fork2-node/greet/bin/greet.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var greet = require('../index.js')
+var greet = require('../src/index.coffee')
 var parsedArgv = require('minimist')(process.argv.slice(2))
 
 console.log(process.argv)
--- fork2-node/greet/test/greet_spec.js
+++ fork2-node/greet/test/greet_spec.js
@@ -1,5 +1,5 @@
 describe('greet', function() {
-	greet = require('../index')
+	greet = require('../src/index.coffee')
 	it("shousd greet a person by name", function() {
     	assert.equal(greet('gerorim'), "Hello gerorim")
 	})
