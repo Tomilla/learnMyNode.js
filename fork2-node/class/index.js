a8346ea593a90eac435c5ce829b01415497440d6
Tomilla <gero.rim@gmail.com>
2014-05-02 10:46:29 +0800

    internal implement of Instance Methods

---
+++ fork2-node/class/index.js
@@ -0,0 +1,11 @@
+module.exports = function(incomes) {
+    this.constructor = incomes.initialize ? incomes.initialize : function () {};
+    for (var mapping in incomes) {
+        if (typeof incomes[mapping] == "function" && mapping != "initialize")
+			this.constructor.prototype[mapping] = incomes[mapping];
+		}
+//    internal implement of Instance Method
+//    this.constructor.prototype["getA"] = incomes["getA"];
+//    this.constructor.prototype["getB"] = incomes["getB"];
+    return this.constructor;
+};
