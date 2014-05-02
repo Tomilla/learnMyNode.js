57d79c8ad6f92d5c02abd04fc16c4adc93e97a27
Tomi lla <Tomilla@users.noreply.github.com>
2014-05-02 11:01:44 +0800

    Update index.js

--- fork2-node/class/index.js
+++ fork2-node/class/index.js
@@ -2,7 +2,7 @@ module.exports = function(incomes) {
     this.constructor = incomes.initialize ? incomes.initialize : function () {};
     for (var mapping in incomes) {
         if (typeof incomes[mapping] == "function" && mapping != "initialize")
-			this.constructor.prototype[mapping] = incomes[mapping];
+            this.constructor.prototype[mapping] = incomes[mapping];
 		}
 //    internal implement of Instance Method
 //    this.constructor.prototype["getA"] = incomes["getA"];
